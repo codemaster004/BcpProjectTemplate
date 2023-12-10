724a3ac1cd95d1ca4db7f24e3fdd84da893651fc
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-10 22:56:52 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+	return 0;
+}
+
+/*
+ * TODO [ ]: Basic UI. (Board outline, space for some text, key binds (Esc, N))
+ * TODO [ ]: 1 game lvl. (floor, several "platforms", "ladders")
+ * TODO [ ]: Moving character (use arrows to move, move vert only on ladders)
+ * TODO [ ]: Display the time passed in game
+ * TODO [ ]: A. Jumps (some settings here) *C
+ * TODO [ ]: B. At least 3 game Lvl, switchable by Num keys
+ * TODO [ ]: C. Barrels exists and move (have some tweak-able values and some logic)
+ * TODO [ ]: D. A LOT
+ * 	  TODO [ ]: Starter Screen menu (Exit, Save, Read, Check results, Select a stage)
+ * 	  TODO [ ]: Entering player names (vis text, ability to delete)
+ * 	  TODO [ ]: Display number of lives
+ * 	  TODO [ ]: Die if touch a barrel
+ * 	  TODO [ ]: If die show some points *F
+ * 	  TODO [ ]: Out of lives show main menu *H (save game?)
+ * 	  TODO [ ]: Get to top next stage
+ * TODO [ ]: E. Animations
+ *    TODO [ ]: Animation for climbing
+ *    TODO [ ]: Jumping
+ *    TODO [ ]: Barrels
+ * TODO [ ]: F. Counting
+ *    TODO [ ]: Keep count of number of Points
+ *    TODO [ ]: Keep points between stages
+ *    TODO [ ]: extra points for jumping over barrels
+ */
+
+/*
+ * ? What is stage?
+ * ? "The time should be displayed along with the information about implemented requirements." yyy wtf?
+ * ? "Character movement and control parameters should be easy to change to achieve smooth control." wtf?
+ * ? Menu: Read
+ * ? Menu: Check results
+ * ? Menu: Save, save what?
+ * ? Is there any particular order we are supposed to complete those requirements?
+ * ? what does animation mean? do we have to prepare a few versions of a "resource" (i.e. character) and be changing them
+ * ?
+ */
